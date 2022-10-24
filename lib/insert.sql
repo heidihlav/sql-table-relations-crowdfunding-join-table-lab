49985a858352d33bf6666fd2d3b886b3b4019df9
Heidi Hlavinka <hchlav@gmail.com>
2022-10-24 17:33:40 -0500

    commit tree febc810bba333f24ceb0310a0dd10dfbc33dcf97

---
+++ lib/insert.sql
@@ -0,0 +1,66 @@
+INSERT INTO users VALUES (1, "Adam", 16);
+INSERT INTO users VALUES (2, "Stacy", 4);
+INSERT INTO users VALUES (3, "Harry", 77);
+INSERT INTO users VALUES (4, "Bhindi", 35);
+INSERT INTO users VALUES (5, "Blah blah", 67);
+INSERT INTO users VALUES (6, "Why", 23);
+INSERT INTO users VALUES (7, "This", 6);
+INSERT INTO users VALUES (8, "Is", 56);
+INSERT INTO users VALUES (9, "So", 9);
+INSERT INTO users VALUES (10, "Much", 5);
+INSERT INTO users VALUES (11, "Repetitive", 54);
+INSERT INTO users VALUES (12, "Work", 24);
+INSERT INTO users VALUES (13, "I", 94);
+INSERT INTO users VALUES (14, "Don't", 45);
+INSERT INTO users VALUES (15, "Eat", 89);
+INSERT INTO users VALUES (16, "Donuts", 34);
+INSERT INTO users VALUES (17, "Anymore", 65);
+INSERT INTO users VALUES (18, "Because", 2);
+INSERT INTO users VALUES (19, "They", 7);
+INSERT INTO users VALUES (20, "Suck", 8);
+
+INSERT INTO projects VALUES (1, "Save the bees.", "environmental", 3000, "2013-06-30", "2013-06-30");
+INSERT INTO projects VALUES (2, "They are written as strings of consecutive alphanumeric characters.", "Elementary, my dear Watson.", 206.496270392967, "07/29/2000", "11/14/2022");
+INSERT INTO projects VALUES (3,	"Its main implementation is the Glasgow Haskell Compiler.", "One morning I shot an elephant in my pajamas. How he got in my pajamas, I don't know.", 23.52660863286050,	"10/12/2016", "12/07/2005");
+INSERT INTO projects VALUES (4,	"Its main implementation is the Glasgow Haskell Compiler.",	"I'm gonna make him an offer he can't refuse.", 141.703228639112, "07/11/2020", "12/16/2018");
+INSERT INTO projects VALUES (5, "Make me a sandwich.", "You're gonna need a bigger boat.", 408.7249906224910, "04/30/2018", "07/18/2013");
+INSERT INTO projects VALUES (6,	"The Galactic Empire is nearing completion of the Death Star.", "Let them eat cake.", 468.962960422741, "08/01/2022", "10/18/2011");
+INSERT INTO projects VALUES (7,	"Type classes first appeared in the Haskell programming language.", "Mama always said life was like a box of chocolates. You never know what you're gonna get.", 874.2739349545290, "01/23/2006", "08/02/2000");
+INSERT INTO projects VALUES (8,	"Atoms are used within a program to denote distinguished values.",	"Ask not what your country can do for you; ask what you can do for your country.", 614.685240825203, "12/11/2008", "06/05/2008");
+INSERT INTO projects VALUES (9,	"Do you come here often?", "A census taker once tried to test me. I ate his liver with some fava beans and a nice Chianti.", 963.115669212628, "08/18/2000", "12/29/2022");
+INSERT INTO projects VALUES (10, "The sequential subset of Erlang supports eager evaluation.", "Get your stinking paws off me, you damned dirty ape.", 203.382276227665, "10/01/2022", "03/30/2003");
+
+
+INSERT INTO pledges VALUES (1, 30.00, 6, 4);
+INSERT INTO pledges VALUES (2, 45.00, 2, 6);
+INSERT INTO pledges VALUES (3, 200.00, 5, 10);
+INSERT INTO pledges VALUES (4, 5000.00, 3, 10);
+INSERT INTO pledges VALUES (5, 600.00, 12, 8);
+INSERT INTO pledges VALUES (6, 50.00, 4, 10);
+INSERT INTO pledges VALUES (7, 5000.00, 3, 6);
+INSERT INTO pledges VALUES (8, 400.00, 12, 10);
+INSERT INTO pledges VALUES (9, 9.00, 3, 3 );
+INSERT INTO pledges VALUES (10, 40000.00, 13, 5);
+INSERT INTO pledges VALUES (11, 2.00, 15, 5);
+INSERT INTO pledges VALUES (12, 30.00, 10, 9);
+INSERT INTO pledges VALUES (13, 950.00, 18, 6);
+INSERT INTO pledges VALUES (14, 7000.00, 14, 9);
+INSERT INTO pledges VALUES (15, 2000.00, 7, 3);
+INSERT INTO pledges VALUES (16, 39.00, 3, 9);
+INSERT INTO pledges VALUES (17, 290.00, 4, 10);
+INSERT INTO pledges VALUES (18, 80.00, 12, 3);
+INSERT INTO pledges VALUES (19, 240.00, 15, 3);
+INSERT INTO pledges VALUES (20, 4000.00, 4, 3);
+INSERT INTO pledges VALUES (21, 78.00, 18, 7);
+INSERT INTO pledges VALUES (22, 9000.00, 1, 3);
+INSERT INTO pledges VALUES (23, 3700.00, 9, 2);
+INSERT INTO pledges VALUES (24, 2.00, 4, 4);
+INSERT INTO pledges VALUES (25, 35.00, 15, 8);
+INSERT INTO pledges VALUES (26, 300.00, 18, 6);
+INSERT INTO pledges VALUES (27, 4590.00, 3, 9);
+INSERT INTO pledges VALUES (28, 2948.00, 9, 1);
+INSERT INTO pledges VALUES (29, 2469.00, 16, 8);
+INSERT INTO pledges VALUES (30, 38569.00, 14, 7);
+
+
+
